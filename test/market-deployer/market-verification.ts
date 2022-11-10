f2be5ca3c5d3f950bf39bb347d523a4ec3e03a9f
Lunar Engineering <lunar-eng@moonwell.fi>
2022-11-10 15:40:47 -0800

    work work

--- test/market-deployer/market-verification.ts
+++ test/market-deployer/market-verification.ts
@@ -27,12 +27,12 @@ test("market-deployer", async () => {
   await sleep(5)
 
   try {
-        const tokenToList = "0x511ab53f793683763e5a8829738301368a2411e3"  //"0x27292cf0016e5df1d8b37306b2a98588acbd6fca" // axlATOM
+        const tokenToList = "0x27292cf0016e5df1d8b37306b2a98588acbd6fca" // axlATOM
         const chainlinkAddress = "0x4F152D143c97B5e8d2293bc5B2380600f274a5dd" // ATOM
-        const tokenSymbol = "WELL" //  "axlATOM"
-        const tokenDecimals = 18 //  6
-        const mTokenName = "Moonwell WELL" // "Moonwell axlATOM"
-        const mTokenSymbol = "mWELL" // "maxlATOM"
+        const tokenSymbol = "axlATOM"
+        const tokenDecimals =  6
+        const mTokenName = "Moonwell axlATOM"
+        const mTokenSymbol = "maxlATOM"
         const reserveFactorPercent = 60
         const protocolSeizeSharePercent = 10
         const collateralFactorPercent = 40
