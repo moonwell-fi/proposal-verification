00b07902ab1d7b9f75d1b0f157e38db84b15411f
Lunar Engineering <lunar-eng@moonwell.fi>
2022-11-22 20:30:50 -0800

    prove multi market

--- test/market-deployer/assertCurrentExpectedState.ts
+++ test/market-deployer/assertCurrentExpectedState.ts
@@ -6,13 +6,25 @@ import {
 
 import {ContractBundle} from "@moonwell-fi/moonwell.js";
 
+// TODO: Refactor this somewhere global to use in a MIP.
+async function assertExpectedStateForToken(
+  provider: ethers.providers.JsonRpcProvider,
+  contracts: ContractBundle,
+  token: string,
+  chainlinkFeedForToken: string
+) {
+  console.log(`[+] Asserting market for ${token} does not exist`)
+  await assertMarketIsNotListed(provider, contracts, token)
+  await assertChainlinkFeedIsNotRegistered(provider, contracts, chainlinkFeedForToken)
+}
+
 export async function assertCurrentExpectedState(
   provider: ethers.providers.JsonRpcProvider, 
   contracts: ContractBundle, 
-  tokenToList: string, 
-  chainlinkFeedForToken: string
+  tokensToList: Array<string>, 
+  chainlinkFeedsForTokens: Array<string>
 ){
-    console.log("[+] Asserting market does not exist")
-    await assertMarketIsNotListed(provider, contracts, tokenToList)
-    await assertChainlinkFeedIsNotRegistered(provider, contracts, chainlinkFeedForToken)
+  for (let i = 0; i < tokensToList.length; i++) {
+    await assertExpectedStateForToken(provider, contracts, tokensToList[i], chainlinkFeedsForTokens[i])
+  }
 }
