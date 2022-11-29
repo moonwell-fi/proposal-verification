7b84723e020b2fc567554a96dc61dd5fa3920227
Lunar Engineering <lunar-eng@moonwell.fi>
2022-11-28 16:07:09 -0800

    mip 7 verifier

---
+++ test/mip-7/generateProposalData.ts
@@ -0,0 +1,8 @@
+import { deployAndWireMarket } from '@moonwell-fi/market-deployer'
+import MarketConfiguration from '@moonwell-fi/market-deployer/dist/types/market-configuration';
+import { Environment } from '@moonwell-fi/moonwell.js';
+
+export const generateProposalData = async (
+) => {
+  // TODO: Return generated proposal data and token addresses.
+}
