c48ceee519d4c235ab18cbc071dba7ba8fa962f2
Octavius <97316189+moonwell-octavius@users.noreply.github.com>
2022-11-30 15:56:13 -0500

    More MIP-1 fixes

--- test/mip-1/assertExpectedEndState.ts
+++ test/mip-1/assertExpectedEndState.ts
@@ -1,16 +1,22 @@
 import {ethers} from "ethers";
+import { _ } from 'lodash'
 import {
     assertMarketBorrowIsNOTPaused, assertMarketBorrowIsPaused,
     assertMarketCFIsNonZero, assertMarketCFIsZero, assertMarketRFIsNOTOneHundred, assertMarketRFIsOneHundred,
     assertMarketSupplyingIsNOTPaused, assertMarketSupplyingIsPaused, assertTransferGuardianIsNotPaused
 } from "../../src/verification/assertions";
 
-import {ContractBundle} from "@moonwell-fi/moonwell.js";
+import {ContractBundle, Market} from "@moonwell-fi/moonwell.js";
 
 export async function assertExpectedEndState(contracts: ContractBundle, provider: ethers.providers.JsonRpcProvider){
     console.log("[+] Asserting markets are in an expected state AFTER gov proposal passed")
 
-    for (const [displayTicker, market] of Object.entries(contracts.MARKETS)){
+    const ACTIVE_MARKETS_AT_THIS_BLOCK: {[ticker: string]: Market} = _.pickBy(
+        contracts.MARKETS,
+        ['FRAX', 'GLMR', 'xcDOT', 'USDC.mad', 'ETH.mad', 'BTC.mad']
+    )
+
+    for (const [displayTicker, market] of Object.entries(ACTIVE_MARKETS_AT_THIS_BLOCK)){
         if (['FRAX', 'GLMR', 'xcDOT'].includes(market.assetTicker)){
             // Borrowing is NOT paused
             await assertMarketBorrowIsNOTPaused(provider, contracts, market)
