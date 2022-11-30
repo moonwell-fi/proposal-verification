69d21d5be68042370a45bf8bc44c3ba5cebde019
Octavius <97316189+moonwell-octavius@users.noreply.github.com>
2022-11-30 14:34:06 -0500

    Add MIP-11

---
+++ test/mip-11/assertExpectedEndState.ts
@@ -0,0 +1,29 @@
+import {ethers} from "ethers";
+
+import {ContractBundle} from "@moonwell-fi/moonwell.js";
+import {
+    ECOSYSTEM_RESERVE,
+    EXPECTED_STARTING_WELL_HOLDINGS, F_GLMR_LM,
+    SENDAMTS,
+} from "./vars";
+import {SUBMITTER_WALLET} from "./vars";
+import {
+    assertMarketRewardState,
+} from "../../src";
+import {assertRoundedWellBalance} from "../../src/verification/assertions";
+import {ENDING_MARKET_REWARDS_STATE} from "./vars";
+
+export async function assertExpectedEndState(contracts: ContractBundle, provider: ethers.providers.JsonRpcProvider){
+    console.log("[+] Asserting protocol is in an expected state AFTER gov proposal passed")
+
+    await assertRoundedWellBalance(contracts, provider,
+        SUBMITTER_WALLET,
+        'SUBMITTER_WALLET',
+        SENDAMTS['SUBMITTER_WALLET']
+    )
+
+    await assertMarketRewardState(
+        contracts, provider, ENDING_MARKET_REWARDS_STATE
+    )
+
+}
