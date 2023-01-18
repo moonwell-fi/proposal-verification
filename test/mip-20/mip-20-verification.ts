a3d426ddefb7dc67e8bea77d748f3d5d6e2e1193
Octavius <97316189+moonwell-octavius@users.noreply.github.com>
2023-01-18 18:42:39 -0500

    Add MIP-20

---
+++ test/mip-20/mip-20-verification.ts
@@ -0,0 +1,58 @@
+import {ethers} from 'ethers'
+import {
+    passLatestGovProposal,
+    passGovProposal,
+    setupDeployerAndEnvForGovernance,
+    sleep,
+    startGanache
+} from "../../src";
+
+import {Contracts} from '@moonwell-fi/moonwell.js'
+import {generateProposalData} from "./generateProposalData";
+import {assertCurrentExpectedState} from "./assertCurrentExpectedState";
+import {assertExpectedEndState} from "./assertExpectedEndState";
+import {C_GLMR_APPDEV, F_GLMR_LM} from "./vars";
+
+const FORK_BLOCK = 2_758_890
+
+test("mip-20-verifications", async () => {
+
+    const contracts = Contracts.moonbeam
+
+    const forkedChainProcess = await startGanache(contracts,
+        FORK_BLOCK,
+        'https://rpc.api.moonbeam.network',
+        [F_GLMR_LM, C_GLMR_APPDEV]
+    )
+
+    console.log("Waiting 5 seconds for chain to bootstrap...")
+    await sleep(5)
+
+    try {
+        const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
+
+        // Go transfer WELL to the deployer key from the cGLMRAPPDEV treasury, delegate those well to the deployer,
+        // and assert the deployer has enough voting power to pass a proposal
+        await setupDeployerAndEnvForGovernance(contracts, provider, C_GLMR_APPDEV, FORK_BLOCK)
+
+        // Go pass proposal currently in timelock
+        console.log('[+] Passing MIP-19...')
+        await passLatestGovProposal(contracts, provider)
+
+        await assertCurrentExpectedState(contracts, provider)
+
+        // Generate new proposal data
+        const proposalData = await generateProposalData(contracts, provider)
+
+        // Pass the proposal
+        await passGovProposal(contracts, provider, proposalData)
+
+        // Assert that our end state is as desired
+        await assertExpectedEndState(contracts, provider)
+    } finally {
+        // Kill our child chain.
+        console.log("Shutting down Ganache chain. PID", forkedChainProcess.pid!)
+        process.kill(-forkedChainProcess.pid!)
+        console.log("Ganache chain stopped.")
+    }
+});
---
+++ test/mip-20/vars.ts
@@ -0,0 +1,218 @@
+import {MarketRewardMap, REWARD_TYPES, TokenHoldingsMap} from "../../src";
+import BigNumber from "bignumber.js";
+
+export const ECOSYSTEM_RESERVE = '0x7793E08Eb4525309C46C9BA394cE33361A167ba4'
+export const F_GLMR_LM = '0x6972f25AB3FC425EaF719721f0EBD1Cdb58eE451'
+export const C_GLMR_APPDEV = '0x519ee031E182D3E941549E7909C9319cFf4be69a'
+
+export const SUBMITTER_WALLET = '0x2c4578032aa885C9b4c1Da15e6738D5A0AFb56C4'
+
+export const EXPECTED_STARTING_WELL_HOLDINGS : TokenHoldingsMap = {
+    F_GLMR_LM: 825_112_863,
+    SUBMITTER_WALLET: 0,
+}
+
+export const SENDAMTS = {
+    DEX_REWARDER: 4_038_462,
+    COMPTROLLER: 4_326_924,
+    ECOSYSTEM_RESERVE: 6_057_693,
+    SUBMITTER_WALLET: 100_000,
+}
+
+// Send amounts should be negative for F_GLMR_LM
+SENDAMTS['F_GLMR_LM'] = -1 * (SENDAMTS['SUBMITTER_WALLET'])
+
+export const EXTRA_MARKET_ADDRESSES = {
+    'BUSD.wh': '0x298f2E346b82D69a473BF25f329BDF869e17dEc8'
+}
+
+/*
+
+GLMR Reward Speeds (pending)
+Set new reward speeds on GLMR to match adjustments:
+    GLMR:
+        Supply: 43919477513227500
+        Borrow: 1
+    xcDOT:
+        Supply: 43919477513227500
+        Borrow: 1
+    FRAX:
+        Supply: 45676256613756600
+        Borrow: 1
+    ETH:
+        Supply: 14054232804232800
+        Borrow: 1
+    BTC:
+        Supply: 14054232804232800
+        Borrow: 1
+    USDC:
+        Supply: 14054232804232800
+        Borrow: 1
+WELL Reward Speeds (pending)
+Set new reward speeds on WELL to match adjustments:
+    GLMR:
+        Supply: 447144001831502000
+        Borrow: 1
+    xcDOT:
+        Supply: 447144001831502000
+        Borrow: 1
+    ETH:
+        Supply: 143086080586081000
+        Borrow: 1
+    BTC:
+        Supply: 143086080586081000
+        Borrow: 1
+    FRAX:
+        Supply: 465029761904762000
+        Borrow: 1
+    USDC:
+        Supply: 143086080586081000
+        Borrow: 1
+
+ */
+
+export const STARTING_MARKET_REWARDS_STATE : MarketRewardMap = {
+    'GLMR': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.043919477513227500").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.447144001831502000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'xcDOT': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.045676256613756600").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.447144001831502000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'FRAX': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.043919477513227500").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.465029761904762000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'ETH.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.014054232804232800").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.143086080586081000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'BTC.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.014054232804232800").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.143086080586081000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'USDC.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.014054232804232800").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.143086080586081000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'BUSD.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber(0),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber(0),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+}
+
+export const ENDING_MARKET_REWARDS_STATE : MarketRewardMap = {
+    'GLMR': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.051669973544973600").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.447144001831502000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'xcDOT': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.045469576719576700").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.393486721611722000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'FRAX': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.043402777777777800").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.375600961538462000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'ETH.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.012400793650793700").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.107314560439560000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'BTC.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.028935185185185200").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.250400641025641000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'USDC.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.016534391534391500").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.143086080586081000").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+    'BUSD.wh': {
+        [REWARD_TYPES.NATIVE]: {
+            expectedSupply: new BigNumber("0.008267195767195770").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+        [REWARD_TYPES.GOVTOKEN]: {
+            expectedSupply: new BigNumber("0.071543040293040300").times(1e18),
+            expectedBorrow: new BigNumber(1),
+        },
+    },
+}
