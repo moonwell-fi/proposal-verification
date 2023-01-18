4138a110e56196e74f58642a1a1ecf31ebf12dfc
Octavius <97316189+moonwell-octavius@users.noreply.github.com>
2023-01-18 18:42:39 -0500

    Add MIP-20

--- src/verification/index.ts
+++ src/verification/index.ts
@@ -36,6 +36,40 @@ export type TokenHoldingsMap = {
 }
 
 
+export async function passLatestGovProposal(contracts: ContractBundle, provider: ethers.providers.JsonRpcProvider, signerAddressOrIndex: number | string = 0){
+    console.log('[+] Passing most recent gov proposal pending in the timelock...')
+    const governor = contracts.GOVERNOR.contract.connect(provider.getSigner(signerAddressOrIndex))
+
+    const latestProposal = await governor.proposals(await governor.proposalCount())
+
+    const voteValueYes = 0
+    const voteResult = await governor.castVote(latestProposal.id, voteValueYes)
+    await voteResult.wait()
+    console.log(`[+] Voted for proposal in ${voteResult.hash}`)
+
+    // Delay until voting end by waiting until the end time and mining one more block
+    console.log("[+] Fast forwarding in time...")
+    await provider.send("evm_mine", [latestProposal.endTimestamp.toNumber() + 1]);
+
+    // Queue our proposal
+    const queueResult = await governor.queue(latestProposal.id)
+    await queueResult.wait()
+    console.log(`[+] Queued for Execution in Hash: ${queueResult.hash}`)
+
+    const timelock = contracts.TIMELOCK.contract.connect(provider)
+
+    // Delay for the timelock to complete by waiting for the timelock's delay, and then waiting one more block
+    const delay = await timelock.delay()
+    const lastBlock = await provider.getBlock("latest")
+    await provider.send("evm_mine", [lastBlock.timestamp + delay.toNumber() + 1]);
+
+    // Execute that shit yo
+    const executeResult = await governor.execute(latestProposal.id)
+    await executeResult.wait()
+
+    console.log(`[+] Executed in hash: ${executeResult.hash}`)
+}
+
 export async function passGovProposal(contracts: ContractBundle, provider: ethers.providers.JsonRpcProvider, proposalData: ProposalData, signerAddressOrIndex: number | string = 0, logProposal = true){
     logProposal && console.log("[+] Submitting the following proposal to governance\n", JSON.stringify(proposalData, null ,2), '\n======')
 
@@ -291,7 +325,8 @@ export async function replaceXCAssetWithDummyERC20(provider: ethers.providers.Js
 export async function assertMarketRewardState(
     contracts: ContractBundle,
     provider: ethers.providers.JsonRpcProvider,
-    marketRewardMap: MarketRewardMap
+    marketRewardMap: MarketRewardMap,
+    extraMarketAddresses: any = {}
 ) {
     for (const [assetTicker, data] of Object.entries(marketRewardMap)) {
 
@@ -302,6 +337,7 @@ export async function assertMarketRewardState(
                 assetTicker,
                 data[REWARD_TYPES.GOVTOKEN].expectedSupply,
                 data[REWARD_TYPES.GOVTOKEN].expectedBorrow,
+                extraMarketAddresses
             )
         }
         if (data[REWARD_TYPES.NATIVE]){
@@ -311,6 +347,7 @@ export async function assertMarketRewardState(
                 assetTicker,
                 data[REWARD_TYPES.NATIVE].expectedSupply,
                 data[REWARD_TYPES.NATIVE].expectedBorrow,
+                extraMarketAddresses
             )
         }
     }
@@ -331,7 +368,13 @@ export function nativeTicker(contracts){
     }
 }
 
-export async function addMarketAdjustementsToProposal(contracts : ContractBundle, unitroller : Contract, proposalData: any, marketRewardMap: MarketRewardMap){
+export async function addMarketAdjustementsToProposal(
+    contracts : ContractBundle,
+    unitroller : Contract,
+    proposalData: any,
+    marketRewardMap: MarketRewardMap,
+    extraMarketAddresses: any = {}
+){
     // For each market
     for (const [assetTicker, data] of Object.entries(marketRewardMap)){
         // And each reward type
@@ -342,7 +385,7 @@ export async function addMarketAdjustementsToProposal(contracts : ContractBundle
             await addProposalToPropData(unitroller, '_setRewardSpeed',
                 [
                     EthersBigNumber.from(assetType), // 0 = WELL, 1 = GLMR
-                    contracts.MARKETS[assetTicker].mTokenAddress,
+                    contracts.MARKETS[assetTicker]?.mTokenAddress ?? extraMarketAddresses[assetTicker],
                     EthersBigNumber.from(supplyBorrowData.expectedSupply.toFixed()),
                     EthersBigNumber.from(supplyBorrowData.expectedBorrow.toFixed()),
                 ],
---
+++ test/mip-20/assertCurrentExpectedState.ts
@@ -0,0 +1,39 @@
+import {ethers} from "ethers";
+
+import {ContractBundle} from "@moonwell-fi/moonwell.js";
+import {
+    EXPECTED_STARTING_WELL_HOLDINGS, EXTRA_MARKET_ADDRESSES,
+    F_GLMR_LM,
+    SUBMITTER_WALLET,
+} from "./vars";
+import {assertCurrentExpectedGovTokenHoldings, assertMarketRewardState} from "../../src";
+import {STARTING_MARKET_REWARDS_STATE} from "./vars";
+import {assertDexRewarderRewardsPerSec, assertSTKWellEmissionsPerSecond} from "../../src/verification/assertions";
+import BigNumber from "bignumber.js";
+
+export async function assertCurrentExpectedState(contracts: ContractBundle, provider: ethers.providers.JsonRpcProvider){
+    console.log("[+] Asserting market configurations are in an expected state")
+
+    await assertCurrentExpectedGovTokenHoldings(
+        contracts,
+        provider,
+        EXPECTED_STARTING_WELL_HOLDINGS,
+        {SUBMITTER_WALLET, F_GLMR_LM}
+    )
+
+    await assertDexRewarderRewardsPerSec(contracts, provider,
+        15,
+        8,
+        new BigNumber('1.878004807692310000').times(1e18)
+    )
+
+    await assertSTKWellEmissionsPerSecond(contracts, provider,
+        new BigNumber('2.295339209401710000').times(1e18)
+    )
+
+    // Assert current borrow/supply emissions
+    await assertMarketRewardState(
+        contracts, provider, STARTING_MARKET_REWARDS_STATE,
+        EXTRA_MARKET_ADDRESSES
+    )
+}
