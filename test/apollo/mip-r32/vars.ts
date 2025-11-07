65507c8ff69005c0bf3dad21a58c2207f488ef1f
Ana Bittencourt <anajuliabit@gmail.com>
2025-11-06 17:41:00 -0800

    Remove hardcoded RPC key, use environment variable

--- test/apollo/mip-r32/vars.ts
+++ test/apollo/mip-r32/vars.ts
@@ -3,8 +3,8 @@ export * from '../base-vars'
 // Fork block - using a recent Moonriver block before oracle deprecation
 export const FORK_BLOCK = 3_800_000
 
-// RPC URL
-export const RPC_URL = 'https://edge.goldsky.com/standard/evm/1285?secret=REMOVED_API_KEY'
+// RPC URL - use environment variable or fallback to public RPC
+export const RPC_URL = process.env.MOONRIVER_RPC_URL || 'https://rpc.api.moonriver.moonbeam.network'
 
 // Markets to pause
 export const MARKETS_TO_PAUSE = ['MOVR', 'xcKSM', 'FRAX']
